2e6bf79d2a787808188e3e126bf7138aa24975a1
Jasson Rdz <ja7rodal@gmail.com>
2017-01-16 22:22:55 -0500

    model type and category

---
+++ app/models/type.rb
@@ -0,0 +1,12 @@
+# == Schema Information
+#
+# Table name: types
+#
+#  id         :integer          not null, primary key
+#  type       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Type < ApplicationRecord
+end
