6c100f0791f80dbf875072c45e96526e3c06a727
Jasson Rdz <ja7rodal@gmail.com>
2017-01-21 21:15:03 -0500

    create expense api

--- app/controllers/api/v1/expenses_controller.rb
+++ app/controllers/api/v1/expenses_controller.rb
@@ -3,4 +3,19 @@ class Api::V1::ExpensesController < ApplicationController
 		@expenses = Expense.all
 		render json: @expenses
 	end
+
+	def create
+		expense = Expense.new(expense_params)
+		if expense.save
+			render  status: 201, json: expense
+		else
+			render  status: 422, json: { errors: expense.errors } 
+		end
+	end
+	
+	private
+		def product_params
+			params.require(:expense).permit(:concept, :value, :date, :category_id, :type_id)
+		end
+
 end
