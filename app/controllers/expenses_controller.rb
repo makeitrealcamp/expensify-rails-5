bf660ccfc0f711c2aba488d72c2307d2b462d2a4
francisco15 <franciscohuertas38@gmail.com>
2017-04-09 18:09:40 -0500

    Added filter by category

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -5,6 +5,9 @@ class ExpensesController < ApplicationController
     @expenses = Expense.between(@month..@month.end_of_month).order("date DESC")
     # Filter by type
     @expenses = @expenses.where(type: params[:type]) if params[:type]
+    # Filter by category
+    @expenses = @expenses.where(category_id: params[:category_id]) if params[:category_id]
+    
     @total = @expenses.map(&:amount).reduce(&:+)
     @tab = :expenses
   end
--- app/helpers/expenses_helper.rb
+++ app/helpers/expenses_helper.rb
@@ -4,13 +4,28 @@ module ExpensesHelper
     params[:type] == type
   end
 
-  # In view has to look href="/expenses?type=xxxxxx
+  # In view has to look like href="/expenses?type=xxxxxx
   def link_to_type(type)
     # Search for the type selected if it does not have the class active
     path = expenses_path(request.query_parameters.merge(type: type))
-    # If the type has the active class, it removes it and search without type
+    # If the type has the active class, it removes it and return the list to default state
     path = expenses_path(request.query_parameters.except(:type)) if type_active(type)
     
     link_to type.capitalize, path, class: "list-group-item #{'active' if type_active(type) }"
   end
+
+  #  Method for know if a category was clicked (active)
+  def category_active(category)
+    params[:category_id] == category.id.to_s
+  end
+
+  # In view has to look like if is not active href="/expenses?category_id=xx"
+  def link_to_category(category)
+    # Search for the category selected if it does not have the class active
+    path = expenses_path(request.query_parameters.merge(category_id: category.id))
+     # If the category has the active class, it removes it and return the list to default state
+    path = expenses_path(request.query_parameters.except(:category_id)) if category_active(category)
+    
+    link_to category.name, path, class: "list-group-item #{'active' if category_active(category) }"
+  end 
 end
