26242bc4496a3d3ba65d53cb9974442f75e04c10
francisco15 <franciscohuertas38@gmail.com>
2017-04-09 18:44:39 -0500

    Created the API and API tests

---
+++ app/controllers/api/v1/expenses_controller.rb
@@ -0,0 +1,36 @@
+class Api::V1::ExpensesController < ApplicationController
+  protect_from_forgery with: :null_session
+
+  def index
+    render json: Expense.all
+  end
+
+  def create
+    expense = Expense.new(expense_params)
+    if expense.save
+      render json: expense, status: 201
+    else
+      render json: { errors: expense.errors }, status: 422
+    end
+  end
+
+  def update
+    expense = Expense.find(params[:id])
+    if expense.update(expense_params)
+      render json: expense, status: 200
+    else
+      render json: { errors: expense.errors }, status: 422
+    end
+  end
+
+  def destroy
+    expense = Expense.find(params[:id])
+    expense.destroy
+    head 204
+  end
+
+  private
+    def expense_params
+      params.require(:expense).permit(:type, :date, :concept, :category_id, :amount)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
 
   resources :expenses, only: [:index, :new, :create, :edit, :update, :destroy]
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  # Serve websocket cable requests in-process
-  # mount ActionCable.server => '/cable'
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :expenses, only: [:index, :create, :update, :destroy]
+    end
+  end
 end
